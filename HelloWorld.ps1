72406e4bb36673700955966459e2e12730bd4bea
McFrankle <cowboymike222@gmail.com>
2019-12-14 13:36:48 -0800

    Display the message as a nice scolling marquee.

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,2 +1,8 @@
-# Script to write a message to the console
-Write-Host "Hello, world!"
+# Script to write a scrolling marquee to the console
+$f = {
+	for ($x = ' ' * 52 + (($args |% t*y) -join ' ' * 4); $x-match ' '){
+		Write-Host "`r$(-join ($x=$x-replace'(?<! .*) ')[0..50])" -NoNewLine
+		Start-Sleep -Milliseconds 99
+	}
+}
+&$f 'Hello, world!'
